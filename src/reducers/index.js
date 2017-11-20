1578ba8defe0a4483118bacdec78b75424bedfa2
Eva <eva.tkavc@gmail.com>
2017-11-20 22:17:01 +0000

    prettier code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,9 @@
 // based on the type of action it received ( i.e. action here received type 'BOOKS_SELECTED' (in index.js); action flows when a book is selected)
 
 // But first, import our reducers
-import { combineReducers } from 'redux';
-import BooksReducer from './reducer_books';
-import ActiveBook from './reducer_active_book';
-
+import { combineReducers } from "redux";
+import BooksReducer from "./reducer_books";
+import ActiveBook from "./reducer_active_book";
 
 // For each key ('books' one covers books hence the key name books), we assign a reducer.
 // book: assigned a book reducer and its value will be an array of books.
@@ -15,7 +14,7 @@ import ActiveBook from './reducer_active_book';
 // Any key to the object that we provide to our combined reducers ends up as key on our global state.
 const rootReducer = combineReducers({
   books: BooksReducer,
-    activeBook: ActiveBook
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
